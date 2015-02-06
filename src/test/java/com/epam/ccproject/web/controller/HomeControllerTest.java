01d5673911d1835a21e98026bfca9bcba5d0fa36
Musak Istvan <musak.istvan@gmail.com>
2015-02-06 10:41:29 -0500

    create basic  webapp homepage

---
+++ src/test/java/com/epam/ccproject/web/controller/HomeControllerTest.java
@@ -0,0 +1,23 @@
+package com.epam.ccproject.web.controller;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HomeControllerTest {
+	private HomeController homeController;
+
+	@Before
+	public void init() {
+		homeController = new HomeController();
+	}
+
+	@Test
+	public void shouldReturnHomepageTilesDefinitionName() {
+		String shouldReturnThisHomepageValue = "home";
+		
+		String homepage = homeController.getHomepage();
+
+		assertEquals(homepage, shouldReturnThisHomepageValue);
+	}
+}
